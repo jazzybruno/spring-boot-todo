c96db41f8c166e636c10c3b7b5f772abcd930ada
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2023-02-18 09:07:34 +0200

    Initiating the project

---
+++ src/main/java/rw/jazzyBruno/todo/BoilerplateSwaggerApplication.java
@@ -0,0 +1,13 @@
+package rw.jazzyBruno.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoilerplateSwaggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoilerplateSwaggerApplication.class, args);
+    }
+
+}
